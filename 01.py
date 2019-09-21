d55d85c76aeccf7d95b045456d5eaa94c220703b
peishichang <peishichang@qq.com>
2019-09-21 20:27:11 +0800

    018

--- 01.py
+++ 01.py
@@ -4,21 +4,31 @@
 # 开发时间: 2019/9/21   16:34
 # 文件名称: 01.py
 # 开发工具: PyCharm
+
+
 class Cat:
+    def __init__(self, new_name):
+        self.name = new_name
+        print('%s来了' % self.name)
+
+    def __del__(self):
+        print('%s去了' % self.name)
+
+    def __str__(self):
+        return '我是小猫[%s]' % self.name
+
     def eat(self):
-        print('%s 爱吃鱼' % self.name)
-    def drink(self):
-        print('%s 爱喝水' % self.name)
+        print('%s爱吃鱼' % self.name)
 
 
-tom = Cat()
-lazy_cat = Cat()
 
-tom.name = 'Tom'
-lazy_cat.name = 'LazyCat'
-tom.eat()
-tom.drink()
-lazy_cat.eat()
-lazy_cat.drink()
+
+tom = Cat('Tom')
+lazy_cat = Cat('Lazy_cat')
+
+print(tom)
+
+del tom
+
 
 
---
+++ xmruning.py
@@ -0,0 +1,17 @@
+# _*_ coding uft-8 _*_
+# 开发团队: 裴和世昌团队
+# 开发人员: 裴世昌
+# 开发时间: 2019/9/21   20:17
+# 文件名称: xmruning.py
+# 开发工具: PyCharm
+
+
+class Person:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return '我的名字叫 %'
+
+
