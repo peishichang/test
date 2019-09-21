d8f356bfa3e9df86db1d0f492c5de7c67133d917
peishichang <peishichang@qq.com>
2019-09-21 16:37:27 +0800

    zuixin

---
+++ 01.py
@@ -0,0 +1,15 @@
+# _*_ coding uft-8 _*_
+# 开发团队: 裴和世昌团队
+# 开发人员: 裴世昌
+# 开发时间: 2019/9/21   16:34
+# 文件名称: 01.py
+# 开发工具: PyCharm
+class Cat:
+    def eat(self):
+        print('小猫爱吃鱼')
+    def drink(self):
+        print('小猫要喝水')
+
+
+tom = Cat()
+lazy_cat = Cat()
--- motto.py
+++
@@ -1,8 +0,0 @@
-print('go big or go home')
-#输出人有悲伤离合
-str1 = '人有悲欢离合'
-print(str1)
-#月有阴晴圆缺
-str2='月有阴晴圆缺'
-print(str2)
-print('test2')
