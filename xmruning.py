2a0b82bbb8f5c656f5e9ebf5b1d27b9c970733b2
peishichang <peishichang@qq.com>
2019-09-21 20:41:00 +0800

    018

--- xmruning.py
+++ xmruning.py
@@ -12,6 +12,22 @@ class Person:
         self.weight = weight
 
     def __str__(self):
-        return '我的名字叫 %'
+
+        return '我的名字叫 %s 体重是 %.2f 公斤' % (self.name, self.weight)
+
+    def run(self):
+        print('%s爱跑步，跑步锻炼身体' %self.name)
+        self.weight -= 0.5
+
+    def eat(self):
+        print('%s是吃货，吃完这顿在减肥' %self.weight)
+        self.weight += 1
+
+xiaoming = Person('小明', 75.0)
+
+xiaoming.run()
+xiaoming.eat()
+
+print(xiaoming)
 
 
