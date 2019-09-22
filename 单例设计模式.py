7e0f4c13368075cfd737351356ca83574953d72f
peishichang <peishichang@qq.com>
2019-09-22 10:46:25 +0800

    单类设计模式完成

--- 单例设计模式.py
+++ 单例设计模式.py
@@ -5,4 +5,29 @@
 # 开发工具: PyCharm
 
 
+class MusicPlayer(object):
+    instance = None
+    init_flag = False
+
+    def __new__(cls, *args, **kwargs):
+        # 1. 判断类属性是否是空对象
+        if cls.instance is None:
+            # 2. 调用父类的方法，为第一个对象分配空间
+            cls.instance = super().__new__(cls)
+        # 3.返回对象的引用
+        return cls.instance
+
+    def __init__(self):
+        if MusicPlayer.init_flag:
+            return
+        MusicPlayer.init_flag = True
+        # 1.判断是否执行过初始化操作
+        print('播放器初始化')
+
+
+# 创建多个对象
+player1 = MusicPlayer()
+print(player1)
+player2 = MusicPlayer()
+print(player2)
 
